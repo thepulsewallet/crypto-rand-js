25d1781dd06bca13e1c245457f3b1c222ac7df76
victornguyenpx <victornguyenpx@gmail.com>
2025-03-12 17:15:37 +0700

    Update library

--- index.ts
+++ index.ts
@@ -3,29 +3,29 @@
 import {Rand} from './lib/rand'
 import {Prime} from './lib/prime'
 import * as BN from 'bn.js'
+import {randomBytes} from 'react-native-randombytes'
 
 Rand.config_randomBytesImp(
-    function (byteSize) {
-        const crypto = require("crypto")
-        return crypto.randomBytes(byteSize);
-    }
+  function (byteSize) {
+    return randomBytes(byteSize) as BN;
+  }
 );
 
 Rand.config_randomPrimeImp(
-    async function (byteSize: number): Promise<BN> {
-        while (true) {
-            let r = await Rand.randomBN(byteSize)
-            if (Prime.isProbablyPrime(r)) {
-                return r
-            }
-        }
-    },
+  async function (byteSize: number): Promise<BN> {
+    while (true) {
+      let r = await Rand.randomBN(byteSize)
+      if (Prime.isProbablyPrime(r)) {
+        return r
+      }
+    }
+  },
 );
 
 Rand.config_randomSafePrimeImp(
-    async function (byteSize: number): Promise<BN> {
-        throw "no implementation for randomSafePrime"
-    },
+  async function (byteSize: number): Promise<BN> {
+    throw "no implementation for randomSafePrime"
+  },
 );
 
 export {Rand, Prime}
